c1addd3c6bb2d0b88c172ff77cd2a8f072e29334
onedayl <onedayleung@gmail.com>
2018-08-23 06:26:00 +0000

    生成 subdomain 表格

--- scripts/subdomain_generate_table.js
+++ scripts/subdomain_generate_table.js
@@ -19,18 +19,13 @@ PortalGenerateRef(reply => {
         reply.data.forEach(el => {
           el.sub.forEach(sub_el => {
 
-            sub_el.factor = (Math.log(sub_el.record) / ref_record) * ref_factor;
+            sub_el.dieoff = Math.log(sub_el.record) / ref_record;
+            sub_el.factor = Math.pow(sub_el.dieoff, 2) * ref_factor;
             sub_el.link = sub_el.record / sub_el.factor;
 
-            if (sub_el.link >= 10) {
-              sub_el.dieoff = sub_el.link < ref_link
-                ? 1 - ref_acceleration * Math.pow(sub_el.link, 2) * 0.5
-                : 1 - ref_acceleration * Math.pow(ref_link - 1, 2) * 0.5;
-              sub_el.impress = parseFloat(((sub_el.pv / sub_el.link) * sub_el.dieoff).toFixed(2));
-
-              if (sub_el.impress >= 10) {
-                data.push(sub_el);
-              }
+            if (sub_el.link >= 5) {
+              sub_el.impress = parseFloat((sub_el.pv * 0.2 / sub_el.link * 0.8).toFixed(2));
+              data.push(sub_el);
             }
           });
         });
@@ -39,11 +34,13 @@ PortalGenerateRef(reply => {
           return `${el.subdomain}, ${el.record}, ${el.percentage}, ${el.pv}, ${el.factor}, ${el.link}, ${el.dieoff}, ${el.impress}`;
         });
         
-        if (!fs.existsSync('public/file')) {
-          fs.mkdirSync('public/file');
+        if (!fs.existsSync('public/files')) {
+          fs.mkdirSync('public/files');
         }
+
+        const headers = 'subdomain, record, percentage, pv, factor, link, dieoff, impress\n';
     
-        fs.writeFileSync('public/file/subdomain.csv', table.join('\n'));
+        fs.writeFileSync('public/files/subdomain.csv', headers + table.join('\n'));
         
       } else {
         console.log(reply.status);
