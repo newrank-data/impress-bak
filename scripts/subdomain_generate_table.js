38c8b936c63abc4bec7d3a9dd72cdf1bb8786c06
onedayl <onedayleung@gmail.com>
2018-08-23 07:24:16 +0000

    修复模块问题

--- scripts/subdomain_generate_table.js
+++ scripts/subdomain_generate_table.js
@@ -2,50 +2,54 @@ const fs = require('fs');
 const PortalGenerateRef = require('./portal_generate_ref.js');
 const SubdomainGenerateList = require('./subdomain_generate_list.js');
 
-PortalGenerateRef(reply => {
+module.exports = function () {
+
+  PortalGenerateRef(reply => {
+    
+    if (reply.status == 1) {
   
-  if (reply.status == 1) {
-
-    const ref_record = Math.log(reply.record);
-    const ref_factor = reply.factor;
-    
-    SubdomainGenerateList(reply => {
+      const ref_record = Math.log(reply.record);
+      const ref_factor = reply.factor;
       
-      if (reply.status == 1) {
-        const data = [];
-
-        reply.data.forEach(el => {
-          el.sub.forEach(sub_el => {
-
-            sub_el.dieoff = Math.log(sub_el.record) / ref_record;
-            sub_el.factor = Math.pow(sub_el.dieoff, 2) * ref_factor;
-            sub_el.link = sub_el.record / sub_el.factor;
-
-            if (sub_el.link >= 5) {
-              sub_el.impress = parseFloat((sub_el.pv * 0.2 / sub_el.link * 0.8).toFixed(2));
-              data.push(sub_el);
-            }
+      SubdomainGenerateList(reply => {
+        
+        if (reply.status == 1) {
+          const data = [];
+  
+          reply.data.forEach(el => {
+            el.sub.forEach(sub_el => {
+  
+              sub_el.dieoff = Math.log(sub_el.record) / ref_record;
+              sub_el.factor = Math.pow(sub_el.dieoff, 2) * ref_factor;
+              sub_el.link = sub_el.record / sub_el.factor;
+  
+              if (sub_el.link >= 5) {
+                sub_el.impress = parseFloat((sub_el.pv * 0.2 / sub_el.link * 0.8).toFixed(2));
+                data.push(sub_el);
+              }
+            });
           });
-        });
-
-        const table = data.map(el => {
-          return `${el.subdomain}, ${el.record}, ${el.percentage}, ${el.pv}, ${el.factor}, ${el.link}, ${el.dieoff}, ${el.impress}`;
-        });
-        
-        if (!fs.existsSync('public/files')) {
-          fs.mkdirSync('public/files');
+  
+          const table = data.map(el => {
+            return `${el.subdomain}, ${el.record}, ${el.percentage}, ${el.pv}, ${el.factor}, ${el.link}, ${el.dieoff}, ${el.impress}`;
+          });
+          
+          if (!fs.existsSync('public/files')) {
+            fs.mkdirSync('public/files');
+          }
+  
+          const headers = 'subdomain, record, percentage, pv, factor, link, dieoff, impress\n';
+      
+          fs.writeFileSync('public/files/subdomain.csv', headers + table.join('\n'));
+          
+        } else {
+          console.log(reply.status);
         }
-
-        const headers = 'subdomain, record, percentage, pv, factor, link, dieoff, impress\n';
-    
-        fs.writeFileSync('public/files/subdomain.csv', headers + table.join('\n'));
-        
-      } else {
-        console.log(reply.status);
-      }
-    });
-
-  } else {
-    console.log(reply.status);
-  }
-});
+      });
+  
+    } else {
+      console.log(reply.status);
+    }
+  });
+  
+}
