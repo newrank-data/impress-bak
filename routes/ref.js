68da4cedafa2c32f3931ef295e25e9ec9b1dd151
onedayl <onedayleung@gmail.com>
2018-10-10 19:08:34 +0800

    修复生成 ref 抛出异常的我难题

--- routes/ref.js
+++ routes/ref.js
@@ -4,24 +4,22 @@ const fs = require('fs');
 const generateRef = require('../scripts/generate_ref.js');
 
 router.get('/', (req, res) => {
-  fs.readFile('public/files/ref.json', (err, ref) => {
-    if (!err) {
-      // 本地存在时直接返回
-      res.status(200).type('application/json').send(ref);
+  try {
+    const ref = fs.readFileSync('public/files/ref.json');
+    res.status(200).type('application/json').send(ref);
 
-    } else {
-      // 本地不存在则立刻生成
-      generateRef(reply => {
-        if (reply.status == 1) {
-          const ref = reply.ref;
-          res.status(200).type('application/json').send(JSON.stringify(ref));
-          
-        } else {
-          res.status(500).type('application/json').send({msg: '生成 ref 失败'});
-        }
-      });
-    }
-  });
+  } catch (err) {
+    generateRef(reply => {
+      if (reply.status == 1) {
+        const ref = reply.ref;
+        res.status(200).type('application/json').send(JSON.stringify(ref));
+        
+      } else {
+        res.status(500).type('application/json').send({msg: '生成 ref 失败'});
+      }
+    });
+  }
+
 });
 
 module.exports = router;
